963ed5e564bb88d83d26360f402106b2e6391fd2
xman <106719630+Xmanrachman@users.noreply.github.com>
2024-12-06 10:23:22 +0700

    Delete SIMS-PPOB-manager directory

--- SIMS-PPOB-manager/src/main/java/com/nutech/test/sims/ppob/authapi/interfaces/UserServiceInterface.java
+++
@@ -1,8 +0,0 @@
-package com.nutech.test.sims.ppob.authapi.interfaces;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserServiceInterface {
-	UserDetailsService userDetailsService();
-
-}
